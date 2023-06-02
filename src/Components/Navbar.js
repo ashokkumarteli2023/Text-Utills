d9924d4ea06caab2085a0f833a099947fac7bc76
ashokkumarteli2023 <106800957+ashokkumarteli2023@users.noreply.github.com>
2023-06-02 16:39:06 +0530

    auto refreshing eliminated

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -50,7 +50,7 @@ export default function Navbar(props) {
       </ul>
       <form className="d-flex" role="search">
         {/* <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" /> */}
-        <button className="btn btn-primary" onClick={toggleMode}>{mode}</button>
+        <button className="btn btn-primary" type="button" onClick={toggleMode}>{mode}</button>
       </form>
     </div>
   </div>
