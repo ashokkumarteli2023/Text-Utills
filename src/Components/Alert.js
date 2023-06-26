e71101a71a91cd13531fc33f9776569223547b88
ashokkumarteli2023 <106800957+ashokkumarteli2023@users.noreply.github.com>
2023-06-26 08:03:36 +0530

    Updated Alert component

--- src/Components/Alert.js
+++ src/Components/Alert.js
@@ -1,14 +1,17 @@
-import React from 'react'
+import React from 'react';
 
 export default function Alert(props) {
     const Capitalize=(word)=>{
         return word.charAt(0).toUpperCase() + word.slice(1);
     }
   return (
-      props.alert && <div className={`alert alert-${props.alert.Type} alert-dismissible fade show`} role="alert">
-          <strong>{Capitalize(props.alert.Type)}</strong>{props.alert.Message}
-          {/* <button type="button" className="btn-close" data-bs-dismiss="alert" aria-label="Close"></button> */}
-      </div>
+      <div style={{ height: "50px" }}>
+            {props.alert && <div className={`alert alert-${props.alert.Type} alert-dismissible fade show`} role="alert">
+                <strong>{Capitalize(props.alert.Type)}</strong>{props.alert.Message}
+                {/* <button type="button" className="btn-close" data-bs-dismiss="alert" aria-label="Close"></button> */}
+            </div>
+            }
+        </div>
   )
 }
 
